a88c546f022f704e21e2094c06aa457f1d6e07c3
Gill Chan <jiahao0408@gmail.com>
2019-03-28 09:05:14 +0800

    ModuleManager

--- BeeHive-Swift/BeeHive/BHModuleManager.swift
+++ BeeHive-Swift/BeeHive/BHModuleManager.swift
@@ -18,4 +18,32 @@ class BHModuleManager {
     func triggerEvent(_ eventType: BHModuleEventType, param: [String: Any]? = nil) {
         
     }
+    func loadLocalModules() {
+        guard let path = Bundle.main.path(forResource: BHContext.shared.moduleName, ofType: "plist"),
+            FileManager.default.fileExists(atPath: path) else { return }
+        let moduleList = NSDictionary(contentsOfFile: path)
+        
+        
+        
+//        NSString *plistPath = [[NSBundle mainBundle] pathForResource:[BHContext shareInstance].moduleConfigName ofType:@"plist"];
+//        if (![[NSFileManager defaultManager] fileExistsAtPath:plistPath]) {
+//            return;
+//        }
+//
+//        NSDictionary *moduleList = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+//
+//        NSArray<NSDictionary *> *modulesArray = [moduleList objectForKey:kModuleArrayKey];
+//        NSMutableDictionary<NSString *, NSNumber *> *moduleInfoByClass = @{}.mutableCopy;
+//        [self.BHModuleInfos enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//            [moduleInfoByClass setObject:@1 forKey:[obj objectForKey:kModuleInfoNameKey]];
+//            }];
+//        [modulesArray enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//            if (!moduleInfoByClass[[obj objectForKey:kModuleInfoNameKey]]) {
+//            [self.BHModuleInfos addObject:obj];
+//            }
+//            }];
+    }
+    func registedAllModules() {
+        
+    }
 }
